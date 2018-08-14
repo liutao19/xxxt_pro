14f550b7d4ed7234797db8daeb49be7de2a16e38
mysupergit <3012477662@qq.com>
2018-08-14 10:10:39 +0800

    pushIOrderSendoutService.java

--- dce-service/src/main/java/com/dce/business/service/order/IOrderSendoutService.java
+++ dce-service/src/main/java/com/dce/business/service/order/IOrderSendoutService.java
@@ -7,41 +7,38 @@ import java.util.Map;
 import com.dce.business.entity.order.OrderSendoutDo;
 import com.dce.business.entity.page.PageDo;
 
-
-
-public interface IOrderSendoutService{
+public interface IOrderSendoutService {
 
 	/**
 	 * 根据ID 查询
+	 * 
 	 * @parameter id
 	 */
 	public OrderSendoutDo getById(Long id);
-	
+
 	/**
-	 *根据条件查询列表
+	 * 根据条件查询列表
 	 */
 	public List<OrderSendoutDo> selectOrderSendout(Map example);
-	
+
 	/**
 	 * 更新
 	 */
-	public int  updateOrderSendoutById(OrderSendoutDo newOrderSendoutDo);
-	
+	public int updateOrderSendoutById(OrderSendoutDo newOrderSendoutDo);
+
 	/**
 	 * 新增
 	 */
 	public int addOrderSendout(OrderSendoutDo newOrderSendoutDo);
-	
-	// /**
-	// * 分页查询
-	// * @param param
-	// * @param page
-	// * @return
-	// */
-	// public PageDo<OrderSendoutDo> getOrderSendoutPage(Map<String, Object>
-	// param, PageDo<OrderSendoutDo> page);
-	
-	
+
+	 /**
+	 * 分页查询
+	 * @param param
+	 * @param page
+	 * @return
+	 */
+	public PageDo<OrderSendoutDo> getOrderSendoutPage(Map<String, Object> param, PageDo<OrderSendoutDo> page);
+
 	/**
 	 * 删除
 	 */

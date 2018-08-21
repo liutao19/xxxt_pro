76fc56ca9cda5bedf88f0fddc20ed6c753347b23
emmsybil <1242776362@qq.com>
2018-08-21 17:17:09 +0800

    pay

--- dce-core/src/main/java/com/dce/business/common/wxPay/util/WXPayConfig.java
+++ dce-core/src/main/java/com/dce/business/common/wxPay/util/WXPayConfig.java
@@ -60,7 +60,7 @@ public class WXPayConfig {
   	public static String ERRORCODE = "errcode";//用来判断access_token是否失效的值
   	public static String SIGN_METHOD = "sha1";//签名算法常量值
   	//package常量值
-  	//public static String packageValue = "bank_type=WX&body=%B2%E2%CA%D4&fee_type=1&input_charset=GBK&notify_url=http%3A%2F%2F127.0.0.1%3A8180%2Ftenpay_api_b2c%2FpayNotifyUrl.jsp&out_trade_no=2051571832&partner=1900000109&sign=10DA99BCB3F63EF23E4981B331B0A3EF&spbill_create_ip=127.0.0.1&time_expire=20131222091010&total_fee=1";
+  	public static String packageValue = "bank_type=WX&body=%B2%E2%CA%D4&fee_type=1&input_charset=GBK&notify_url=http%3A%2F%2F127.0.0.1%3A8180%2Ftenpay_api_b2c%2FpayNotifyUrl.jsp&out_trade_no=2051571832&partner=1900000109&sign=10DA99BCB3F63EF23E4981B331B0A3EF&spbill_create_ip=127.0.0.1&time_expire=20131222091010&total_fee=1";
   	public static String traceid = "testtraceid001";//测试用户id
 	
 }

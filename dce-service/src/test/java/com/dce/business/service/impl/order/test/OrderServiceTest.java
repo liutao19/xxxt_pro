e94f31912e8d8893cce28ff945c91d1aaab7c2e7
emmsybil <1242776362@qq.com>
2018-08-23 21:11:57 +0800

    alipay

---
+++ dce-service/src/test/java/com/dce/business/service/impl/order/test/OrderServiceTest.java
@@ -0,0 +1,21 @@
+package com.dce.business.service.impl.order.test;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dce.business.service.order.IOrderService;
+import com.dce.test.BaseTest;
+
+public class OrderServiceTest  extends BaseTest{
+	
+	@Autowired
+	private IOrderService orderService;
+	
+	@Test
+	public void testOrderPay(){
+		String gmtPayment = "2018-08-23";
+		String orderCode = "XX3820180823135359745";
+		orderService.orderPay(orderCode , gmtPayment);
+	}
+
+}
